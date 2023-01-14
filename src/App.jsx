212556419796748550e55653ae230384e9896a09
Reem <reembsrat@gmail.com>
2023-01-14 11:50:01 +0300

    search working on typing

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,38 @@ import Data from "../mock-data.json"
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [input,setInput]=useState("")
+  const [input, setInput] = useState("")
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log("submit")
+  }
 
   return (
     <div className="App">
-     <input placeholder='Enter book name,author,date,...' onChange={(e)=> setInput(e.target.value)} />
-
-     {
-      Data.filter((book)=>{
-        if(input==""){
-          // if input is empty
-          return book
-        }else if(book.title.toLowerCase().includes(input.toLowerCase())){
-          return book
-        }
-      })
-      .map((book)=>(
-        <div className="box" key={book.id}>
-          <p>{book.title}</p>
-          <p>{book.author}</p>
+      <form className="search-bar" onSubmit={onSubmit}>
+        <div className="form-control">
+          <input type="text" placeholder='Enter book name,author,date,...' onChange={(e)=> setInput(e.target.value)} />
         </div>
-      ))
-     }
+        <input value="Submit" type="submit"  />
+      </form>
+
+      {
+        Data.filter((book) => {
+          if (input == "") {
+            // if input is empty
+            return book
+          } else if (book.title.toLowerCase().includes(input.toLowerCase())) {
+            return book
+          }
+        })
+          .map((book) => (
+            <div className="box" key={book.id}>
+              <p>{book.title}</p>
+              <p>{book.author}</p>
+            </div>
+          ))
+      }
     </div>
   )
 }
